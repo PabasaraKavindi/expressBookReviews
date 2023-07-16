753739a8f214e7e9c1c28c43984b38626a5dcfc9
IT21052774 <it21052774@my.sliit.lk>
2023-07-16 16:03:22 +0530

    complete project

--- final_project/index.js
+++ final_project/index.js
@@ -10,8 +10,25 @@ app.use(express.json());
 
 app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
 
+
+// Updating the code for the authentication mechanism: , Use the session authorization feature (implemented in the Practice project lab) to authenticate a user based on the access token.
+
 app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
+    if(req.session.authorization) {
+        token = req.session.authorization['accessToken'];
+        jwt.verify(token, "access",(err,user)=>{
+            if(!err){
+                req.user = user;
+                next();
+            }
+            else{
+                return res.status(403).json({message: "User not authenticated"})
+            }
+         });
+     } else {
+         return res.status(403).json({message: "User not logged in"})
+     }
+
 });
  
 const PORT =5000;
